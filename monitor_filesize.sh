42127243ea54782725a98b5e6bb6fc72f1172848
satou.akira <satoach.net@gmail.com>
2023-04-07 20:54:12 +0900

    file size

--- monitor_filesize.sh
+++ monitor_filesize.sh
@@ -8,10 +8,11 @@ fi
 
 file=$1
 
+OPT=modify,attrib,close_write,move,create,delete
 while true
 do
-    while inotifywait -e modify,attrib,close_write,move,create,delete $file; do
-        ls -lh $file
+    while inotifywait -e $OPT $file &> /dev/null; do
+        echo `date +%H%M%S` `ls -lh $file | cut -d ' ' -f5 -`
     done
 done
 
