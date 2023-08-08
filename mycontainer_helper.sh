4de1ebe974b6e5d9233ec2f52cbf8efdbcdb0530
satou.akira <satoach.net@gmail.com>
2023-08-08 11:41:01 +0900

    use mycommondef

--- mycontainer_helper.sh
+++ mycontainer_helper.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 
-readonly RED="$(tput setaf 1)"
-readonly GREEN="$(tput setaf 2)"
-readonly NORMAL="$(tput sgr0)"
+source mycommondef.sh
 
 CMD=
 CONTAINER=
-
-fatal()
-{
-    echo -e "${RED} $@ ${NORMAL}"
-    exit 1
-}
-
-function help() ## print help
-{
-    echo -e "${GREEN} HELP $0 ${NORMAL}"
-    echo "----------------------------------------"
-    grep -E '^function [a-zA-Z_-]+ *\( *\) *## .*$$' $0 | cut -d " " -f2- | sort \
-        | awk ' \
-             BEGIN {FS = "\\( *\\) *##"} \
-             {args = ""; breif = ""} \
-             match($2, /a:.+b:/) {args = substr($2, RSTART+2, RLENGTH-2-2)} \
-             # {printf "\033[33m%s\033[36m%s \033[0m\n\t%s\n",\
-             {printf "%s %s\n\t%s\n",\
-                        $1, args, substr($2, RSTART+RLENGTH), length($2)-RSTART}'
-}
 
 select_cmd()
 {
@@ -73,6 +51,14 @@ function stop() ## stop the container
     ${CMD} stop ${CONTAINER}
 }
 
+function restart() ## restart the container
+{
+    select_container $1
+    ${CMD} stop ${CONTAINER}
+    ${CMD} stop ${CONTAINER}
+    ${CMD} start ${CONTAINER}
+}
+
 function show()  ## show the current container list
 {
     echo "---------------------------"
@@ -89,6 +75,7 @@ function show()  ## show the current container list
 case $1 in
     "start") start ${@:2};;
     "stop") stop ${@:2};;
+    "restart") restart ${@:2};;
     "show") show;;
     *) help;;
 esac
