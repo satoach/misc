f8b3c1d57c85d0235c8e5261071059015fe05041
satou.akira <satoach.net@gmail.com>
2025-05-30 09:48:25 +0900

    build(mk): apply NO_COLOR option in help

--- myutil.mk
+++ myutil.mk
@@ -2,14 +2,24 @@ ifndef INCLUDED_MYUTIL_MK
 INCLUDED_MYUTIL_MK := 1
 
 SHELL := /bin/bash
-print_val = @echo $(1) is $($(1))
+
 nproc := $(shell nproc)
+print_val = @echo $(1) is $($(1))
+
+# 色定義（NO_COLOR が設定されていれば空文字になる）
+ifeq ($(NO_COLOR),)
+COLOR_CYAN := \033[36m
+COLOR_RESET := \033[0m
+else
+COLOR_CYAN :=
+COLOR_RESET :=
+endif
 
 help:  ## Print help
 	@echo "USAGE (makefile for using my vim's quickfix):"
 	@grep -h -E '^[a-zA-Z_%-]+:.*?## .*$$' $(MAKEFILE_LIST) | \
 	sort -u | \
-	awk 'BEGIN {FS = ":.*?## "}; {printf "\t\033[36m%-20s\033[0m %s\n", $$1, $$2}'
+	awk 'BEGIN {FS = ":.*?## "}; {printf "\t$(COLOR_CYAN)%-20s$(COLOR_RESET) %s\n", $$1, $$2}'
 	@echo ""
 
 .PHONY: help
