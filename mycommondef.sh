259c555d5bc56039e59041c195d0ed84a2f8f3a1
satou.akira <satoach.net@gmail.com>
2025-07-18 17:29:52 +0900

    chore: update mycommondef

--- mycommondef.sh
+++ mycommondef.sh
@@ -4,22 +4,38 @@ readonly RED="$(tput setaf 1)"
 readonly GREEN="$(tput setaf 2)"
 readonly NORMAL="$(tput sgr0)"
 
-fatal()
-{
+fatal() {
     echo -e "${RED} $@ ${NORMAL}"
     exit 1
 }
 
-function help() ## print help
+function help()  ## print help
 {
+    eval files=$(grep -E '^(source|\.)\s+([^[:space:]]+)' "$0" |
+        awk '{print $2}' |
+        sort -u)
+
     echo -e "${GREEN} HELP $0 ${NORMAL}"
     echo "----------------------------------------"
-    grep -E '^function [a-zA-Z_-]+ *\( *\) *## .*$$' $0 | cut -d " " -f2- | sort \
-        | awk ' \
-             BEGIN {FS = "\\( *\\) *##"} \
-             {args = ""; breif = ""} \
-             match($2, /a:.+b:/) {args = substr($2, RSTART+2, RLENGTH-2-2)} \
-             # {printf "\033[33m%s\033[36m%s \033[0m\n\t%s\n",\
-             {printf "%s %s\n\t%s\n",\
-                        $1, args, substr($2, RSTART+RLENGTH), length($2)-RSTART}'
+    grep -E '^function [a-zA-Z_-]+ *\( *\) *## .*$$' $0 "$files" | cut -d " " -f2- | sort |
+        awk '
+            BEGIN { FS = "\\( *\\) *##" }
+            {
+              args = ""
+              desc = ""
+            
+              # 説明部分の抜き出し
+              n = split($2, parts, /var:[ \t]*[a-zA-Z0-9_]+/)
+              desc = parts[1]
+            
+              # var:x var:y ... をすべてマッチ
+              while (match($2, /var:[ \t]*[a-zA-Z0-9_]+/)) {
+                arg = substr($2, RSTART+4, RLENGTH-4)
+                args = args " " arg
+                $2 = substr($2, RSTART+RLENGTH)
+              }
+            
+              printf "%s%s\n\t%s\n", $1, args, desc
+            }
+        '
 }
